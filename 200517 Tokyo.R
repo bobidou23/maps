a079be1f8bfd0adb6ba545b7634e315ad493614d
bobidou23 <kshiromoto0723@gmail.com>
2021-04-14 00:01:54 +0900

    figured out the distance plots

--- 200517 Tokyo.R
+++ 200517 Tokyo.R
@@ -21,23 +21,138 @@ for (i in 1:47) {
   unzip(temp)
 }
 
-kanto <- rbind(read_sf("~/Desktop/Datasets/Japan/h27ka08.shp"),
-               read_sf("~/Desktop/Datasets/Japan/h27ka09.shp"),
-               read_sf("~/Desktop/Datasets/Japan/h27ka10.shp"),
-               read_sf("~/Desktop/Datasets/Japan/h27ka11.shp"),
-               read_sf("~/Desktop/Datasets/Japan/h27ka12.shp"),
-               read_sf("~/Desktop/Datasets/Japan/h27ka13.shp"),
-               read_sf("~/Desktop/Datasets/Japan/h27ka14.shp"))%>%
+japan_geo <- data.frame()
+for(i in 1:47) {
+  japan_geo <- rbind(japan_geo, read_sf(paste0("~/Desktop/Datasets/Japan/h27ka",sprintf("%02s", i),".shp")))
+} %>%
   mutate(density = JINKO/AREA*1000)
 setwd("~/Dropbox/Projects/maps")
 
-kanto %>%
-  filter(is.na(SITYO_NAME),
-         JINKO > 0) %>%
+kanto <- filter(japan_geo, PREF_NAME %in% c("群馬県","栃木県","茨城県","埼玉県","千葉県","東京都","神奈川県")) %>%
+  mutate(centroid = st_centroid(geometry),
+         radius = as.numeric(st_distance(centroid,
+                                         st_sfc(st_point(x = c(139.767, 35.682), dim = "XY"),crs=4612)))/1000,
+         areaclass = case_when(density == 0 ~ "Zero",
+                               density < 1.5 & density > 0 & radius < 50 ~ "Near low",
+                               density < 1.5 & density > 0 & radius > 50 ~ "Far low",
+                               density > 1.5 & radius > 50 ~ "Far medium",
+                               TRUE ~ "Near"))
+
+kanto %>% #map
+  filter(is.na(SITYO_NAME), #filters out distant islands in Tokyo Prefecture
+         JINKO > 0) %>% #filters out lakes, etc
+  mutate(density = case_when(density > 50 ~ 50, TRUE ~ density)) %>%
+  ggplot() +
+  geom_sf(aes(fill=density), size=0, alpha=0.8) +
+  # xlim(139,140.5) +
+  # ylim(35,36.25) +
+  scale_fill_gradientn(trans = "sqrt", limits=c(0,50), colours = inlmisc::GetColors(256,start=0.2,end=1))
+
+p_load(ggExtra)
+kanto %>% #dotplot
+  filter(is.na(SITYO_NAME)) %>%
+  ggplot() +
+  geom_point(aes(x=radius,y=density,size=JINKO,color=PREF),alpha=0.05) +
+  # geom_hline(yintercept=1) +
+  # geom_segment(aes(x=50,xend=50,y=1,yend=50)) +
+  # scale_x_continuous(trans="sqrt") +
+  scale_y_continuous(trans="sqrt",limits=c(0,50))
+
+kanto %>% #dotplot
+  filter(is.na(SITYO_NAME)) %>%
+  ggplot() +
+  geom_density(aes(y=density,weight=JINKO))
+
+kanto %>% #map
+  filter(is.na(SITYO_NAME)) %>% #filters out distant islands in Tokyo Prefecture
+  mutate(density = case_when(density > 50 ~ 50, TRUE ~ density)) %>%
+  ggplot() +
+  geom_sf(aes(fill=areaclass), size=0, alpha=0.6) +
+  # geom_circle(x0=139.767,y0=35.682,r=0.451) +
+  scale_fill_manual(values=c("Near"="orange","Far medium"="orange3",
+                             "Near low"="lightskyblue1","Far low"="lightskyblue3","Zero"="grey50"))
+
+geom_circle <- function(x0, y0, r, npoints=100, ...) {
+  angles <- seq(-pi, pi, length.out=npoints)
+  x <- sin(angles) * r
+  y <- cos(angles) * r
+  circdat <- data.frame(x=x+x0, y=y+y0)
+  geom_polygon(data=circdat, 
+               mapping=aes(x, y),
+               alpha=0, 
+               colour="black",
+               ...)
+}
+
+##KANSAI
+kansai <- filter(japan_geo, PREF_NAME %in% c("兵庫県","京都府","大阪府","滋賀県","和歌山県","奈良県")) %>%
+  mutate(centroid = st_centroid(geometry),
+         radius = as.numeric(st_distance(centroid,
+                                         st_sfc(st_point(x = c(135.495, 34.702), dim = "XY"),crs=4612)))/1000)
+
+kansai %>% #map
+  filter(is.na(SITYO_NAME), #filters out distant islands in Tokyo Prefecture
+         JINKO > 0) %>% #filters out lakes, etc
   mutate(density = case_when(density > 50 ~ 50, TRUE ~ density)) %>%
   ggplot() +
   geom_sf(aes(fill=density), size=0, alpha=0.8) +
   scale_fill_gradientn(trans = "sqrt", limits=c(0,50), colours = inlmisc::GetColors(256,start=0.2,end=1))
+
+kansai %>% #dotplot
+  filter(is.na(SITYO_NAME)) %>%
+  ggplot() +
+  geom_point(aes(x=radius,y=density,size=JINKO,color=PREF),alpha=0.05) +
+  # geom_hline(yintercept=1) +
+  # geom_segment(aes(x=50,xend=50,y=1,yend=50)) +
+  # scale_x_continuous(trans="sqrt") +
+  scale_y_continuous(trans="sqrt",limits=c(0,50))
+
+##NAGOYA
+nagoya <- filter(japan_geo, PREF_NAME %in% c("愛知県","岐阜県","三重県")) %>%
+  mutate(centroid = st_centroid(geometry),
+         radius = as.numeric(st_distance(centroid,
+                                         st_sfc(st_point(x = c(136.908,35.170), dim = "XY"),crs=4612)))/1000)
+
+nagoya %>% #map
+  filter(is.na(SITYO_NAME), #filters out distant islands in Tokyo Prefecture
+         JINKO > 0) %>% #filters out lakes, etc
+  mutate(density = case_when(density > 50 ~ 50, TRUE ~ density)) %>%
+  ggplot() +
+  geom_sf(aes(fill=density), size=0, alpha=0.8) +
+  scale_fill_gradientn(trans = "sqrt", limits=c(0,50), colours = inlmisc::GetColors(256,start=0.2,end=1))
+
+nagoya %>% #dotplot
+  filter(is.na(SITYO_NAME)) %>%
+  ggplot() +
+  geom_point(aes(x=radius,y=density,size=JINKO,color=PREF),alpha=0.05) +
+  # geom_hline(yintercept=1) +
+  # geom_segment(aes(x=50,xend=50,y=1,yend=50)) +
+  # scale_x_continuous(trans="sqrt") +
+  scale_y_continuous(trans="sqrt",limits=c(0,50))
+
+##FUKUOKA
+fukuoka <- filter(japan_geo, PREF_NAME == "福岡県" | PREF_NAME == "佐賀県") %>%
+  mutate(centroid = st_centroid(geometry),
+         radius = as.numeric(st_distance(centroid,
+                                         st_sfc(st_point(x = c(130.4,33.589), dim = "XY"),crs=4612)))/1000)
+
+fukuoka %>% #map
+  filter(is.na(SITYO_NAME), #filters out distant islands in Tokyo Prefecture
+         JINKO > 0) %>% #filters out lakes, etc
+  mutate(density = case_when(density > 50 ~ 50, TRUE ~ density)) %>%
+  ggplot() +
+  geom_sf(aes(fill=density), size=0, alpha=0.8) +
+  scale_fill_gradientn(trans = "sqrt", limits=c(0,50), colours = inlmisc::GetColors(256,start=0.2,end=1))
+
+fukuoka %>% #dotplot
+  filter(is.na(SITYO_NAME)) %>%
+  ggplot() +
+  geom_point(aes(x=radius,y=density,size=JINKO,color=PREF),alpha=0.05) +
+  # geom_hline(yintercept=1) +
+  # geom_segment(aes(x=50,xend=50,y=1,yend=50)) +
+  # scale_x_continuous(trans="sqrt") +
+  scale_y_continuous(trans="sqrt",limits=c(0,50))
+
 
 ja_lines <- st_read("~/Desktop/Datasets/ja_lines.shp")
 
@@ -58,5 +173,6 @@ wtd.quantile(mutate(kanto, density = JINKO/AREA*10000) %$% density, c(0.25, 0.5,
 {ggplot(kanto) + geom_density(aes(x=density,weight=JINKO),alpha=0.4) +
     xlim(0,500)}
 
-
+p_load(usethis)
+usethis::create_github_token()
 
--- 200521 Canada.R
+++ 200521 Canada.R
@@ -7,7 +7,8 @@ library(rgdal)
 
 #Layers https://libguides.tru.ca/censuscanada/censustract
 
-#Census Tracts + Dissemination Areas (smaller)
+# Census Tracts + Dissemination Areas (smaller)
+# Boundary files https://www12.statcan.gc.ca/census-recensement/2011/geo/bound-limit/bound-limit-2016-eng.cfm
 setwd("~/Desktop/Datasets")
 url <- "http://www12.statcan.gc.ca/census-recensement/2011/geo/bound-limit/files-fichiers/2016/lda_000b16a_e.zip"
 temp <- tempfile()
@@ -19,22 +20,38 @@ can_da <- st_read("lda_000b16a_e.shp")
 can_da2 <- read_csv("https://www12.statcan.gc.ca/census-recensement/2016/dp-pd/hlt-fst/pd-pl/Tables/CompFile.cfm?Lang=Eng&T=1901&OFT=FULLCSV")
 #from https://www12.statcan.gc.ca/census-recensement/2016/dp-pd/prof/details/page_Download-Telecharger.cfm?Lang=E&Tab=1&Geo1=CT&Code1=5050200.01&Geo2=PR&Code2=01&SearchText=5050200.01&SearchType=Begins&SearchPR=01&B1=All&TABID=3&type=0
 
+can_all <- mutate(can_da, "DAUID"=as.double(DAUID)) %>%
+  left_join(can_da2, by=c("DAUID"="Geographic code")) %>%
+  mutate(density = `Population density per square kilometre, 2016`/1000)
+
 setwd("~/Dropbox/Projects/maps")
 
 #Mapping
-mtl <- mutate(can_da, "DAUID"=as.double(DAUID)) %>%
-  left_join(can_da2, by=c("DAUID"="Geographic code")) %>%
-  filter(CMANAME=="Montréal") %>%
-  mutate(density = `Population density per square kilometre, 2016`/1000,
-         density = case_when(density > 50 ~ 50,
-                             TRUE ~ density))
+mtl <- filter(can_all, CMANAME=="Montréal") %>%
+  mutate(density = case_when(density > 50 ~ 50,
+                             TRUE ~ density),
+         category = case_when(CDNAME == "Montréal" ~ "Montréal",
+                              TRUE ~ "Surrounding"),
+         radius = as.numeric(st_distance(st_centroid(geometry),
+                                         st_transform(st_sfc(st_point(x=c(-73.567, 45.5), dim="XY"), crs=4326),
+                                                      crs=st_crs(can_da))))/1000)
 
-toronto <- mutate(can_da, "DAUID"=as.double(DAUID)) %>%
-  left_join(can_da2, by=c("DAUID"="Geographic code")) %>%
-  filter(CMANAME=="Toronto" | CMANAME=="Hamilton" | CMANAME=="Oshawa") %>%
-  mutate(density = `Population density per square kilometre, 2016`/1000,
-         density = case_when(density > 50 ~ 50,
-                             TRUE ~ density))
+toronto <- filter(can_all, CMANAME=="Toronto" | CMANAME=="Hamilton" | CMANAME=="Oshawa") %>%
+  mutate(density = case_when(density > 50 ~ 50,
+                             TRUE ~ density),
+         category = case_when(CDNAME == "Toronto" ~ "Toronto",
+                              TRUE ~ "Surrounding"),
+         radius = as.numeric(st_distance(st_centroid(geometry),
+                                         st_transform(st_sfc(st_point(x=c(-79.379, 43.645), dim="XY"), crs=4326),
+                                                      crs=st_crs(can_da))))/1000)
+
+mtl %>%
+  ggplot()+
+  geom_point(aes(x=radius,y=density,size=`Population, 2016`,color=category),alpha=0.05) +
+  scale_y_continuous(trans="sqrt",limits=c(0,50)) +
+  scale_x_continuous(trans="sqrt")
+
+
 
 vancouver <- mutate(can_da, "DAUID"=as.double(DAUID)) %>%
   left_join(can_da2, by=c("DAUID"="Geographic code")) %>%
@@ -43,7 +60,7 @@ vancouver <- mutate(can_da, "DAUID"=as.double(DAUID)) %>%
          density = case_when(density > 50 ~ 50,
                              TRUE ~ density))
 
-ggplot(toronto) +
+ggplot(mtl) +
   geom_sf(aes(fill=density),size=0,alpha=0.8) +
   scale_fill_gradientn(trans = "sqrt", limits=c(0,50), colours = inlmisc::GetColors(256,start=0.2,end=1))
 
